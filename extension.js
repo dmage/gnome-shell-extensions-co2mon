8b5b22934def4260dc620eefd52f0a1f8ac9aade
Oleg Bulatov <oleg@bulatov.me>
2015-02-02 00:17:44 +0300

    Show temperature
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- extension.js
+++ extension.js
@@ -5,8 +5,9 @@ const Main = imports.ui.main;
 const Gio = imports.gi.Gio;
 // const Tweener = imports.ui.tweener;
 
-// let text, button;
-let button, co2_value, timer;
+let temp_button, temp_value,
+    co2_button, co2_value,
+    proxy;
 
 // function _hideHello() {
 //     Main.uiGroup.remove_actor(text);
@@ -46,9 +47,9 @@ const CO2MonDaemonIface = '<node>\
 
 const CO2MonDaemonProxy = Gio.DBusProxy.makeProxyWrapper(CO2MonDaemonIface);
 
-function init() {
-    button = new St.Bin({
-        style_class: 'panel-button',
+function init_co2() {
+    co2_button = new St.Bin({
+        style_class: 'panel-button co2mon-panel-button',
         reactive: true,
         can_focus: true,
         x_fill: true,
@@ -56,7 +57,7 @@ function init() {
         track_hover: true
     });
 
-    layout = new St.BoxLayout({});
+    let layout = new St.BoxLayout({});
 
     let co2_co = new St.Label({
         style_class: 'co2mon-unit-label',
@@ -71,36 +72,74 @@ function init() {
         text: "---"
     });
 
+    layout.add_child(co2_value);
+    layout.add_child(co2_co);
+    layout.add_child(co2_2);
+    co2_button.set_child(layout);
+    co2_button.connect('button-press-event', function() {
+        co2_value.set_text("click");
+    });
+}
+
+function init_temp() {
+    temp_button = new St.Bin({
+        style_class: 'panel-button co2mon-panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+
+    let layout = new St.BoxLayout({});
+
+    temp_value = new St.Label({
+        style_class: 'co2mon-value-label',
+        text: "---"
+    });
+    let temp_degC = new St.Label({
+        style_class: 'co2mon-unit-label',
+        text: "°C"
+    });
+
+    layout.add_child(temp_value);
+    layout.add_child(temp_degC);
+    temp_button.set_child(layout);
+    temp_button.connect('button-press-event', function() {
+        temp_value.set_text("click");
+    });
+}
+
+function init() {
+    init_temp();
+    init_co2();
+
     // let icon = new St.Icon({ icon_name: 'system-run-symbolic',
     //                          style_class: 'system-status-icon' });
 
-    layout.add_child(co2_co);
-    layout.add_child(co2_2);
-    button.set_child(layout);
-    layout.add_child(co2_value);
-    // button.connect('button-press-event', _showHello);
-    button.connect('button-press-event', function() {
-        co2_value.set_text("click");
-    });
-
-    let proxy = new CO2MonDaemonProxy(
+    proxy = new CO2MonDaemonProxy(
         Gio.DBus.session,
         null, //"io.github.dmage.CO2Mon",
         "/io/github/dmage/CO2Mon"
     );
+}
+
+function enable() {
+    Main.panel._rightBox.insert_child_at_index(co2_button, 0);
+    Main.panel._rightBox.insert_child_at_index(temp_button, 0);
 
     proxy.connectSignal("NewValue", function(proxy, sender, parameters) {
         let [code, raw_value, name, value] = parameters;
-        if (name == "CO2") {
+        if (name == "TEMP") {
+            var rounded = Math.round(value.get_double()*10)/10;
+            temp_value.set_text("" + rounded);
+        } else if (name == "CO2") {
             co2_value.set_text("" + value.get_uint16());
         }
     });
 }
 
-function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
-}
-
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._rightBox.remove_child(temp_button);
+    Main.panel._rightBox.remove_child(co2_button);
 }
